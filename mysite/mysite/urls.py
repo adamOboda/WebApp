f09ff42ebae102c8ca2c693fa50df7b6ca367c3e
Adam <adam.oboda1998@gmail.com>
2022-06-30 21:25:11 +0200

    Branching

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 from polls import views
 
-
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
